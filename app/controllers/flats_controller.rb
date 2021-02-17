818839b52fe3b7c72872d37b93dee968bd99321b
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-02-17 15:54:16 -0300

    flat#edit#update added

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,4 +1,5 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: %i[edit update]
   
   def index
     @flats = Flat.all
@@ -21,7 +22,23 @@ class FlatsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @flat.update(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      :edit
+    end
+  end
+
   private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
 
   def flat_params
     params.require(:flat).permit(:name, :address, :description, :number_of_guests, :price_per_night)
